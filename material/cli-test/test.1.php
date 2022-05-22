3c311a6e8cf1fd705bff61cef23d9e1eca679658
Steven Bühner <buehner@me.com>
2022-05-22 12:54:40 +0200

    Adding some of my Tests

--- material/cli-test/test.1.php
+++ material/cli-test/test.1.php
@@ -3,18 +3,20 @@
 require "bootstrap.php";
 
 use GuzzleHttp\Cookie\CookieJar;
+use StevenBuehner\ChurchTools\Api\CalendarApi;
+use StevenBuehner\ChurchTools\Api\CampusApi;
 use StevenBuehner\ChurchTools\Api\GroupApi;
 use StevenBuehner\ChurchTools\Api\PersonApi;
-use StevenBuehner\ChurchTools\ApiException;
 use StevenBuehner\ChurchTools\Configuration;
 use StevenBuehner\ChurchToolsApi\ChurchToolsUserAuthenticatedClient;
 
 
 // Create Config
 $config = Configuration::getDefaultConfiguration();
-$config->setHost('https://slug.church.tools/api');
-$config->setUsername('username');
-$config->setPassword('password');
+list ($host, $username, $password) = require 'credentials.php';
+$config->setHost($host);
+$config->setUsername($username);
+$config->setPassword($password);
 
 
 // Create Client with Autthentication
@@ -30,12 +32,30 @@ $allPersons = $personApi->getAllPersons(NULL, NULL, NULL, NULL, NULL, NULL, 1, 1
 $firstPerson = $allPersons[0];
 $allGroups   = $groupApi->getAllGroupsForPerson($firstPerson->getId())->getData();
 
+echo "Alle sichtbaren Gruppen des Nutzers '" . $firstPerson->getFirstName() . ' ' . $firstPerson->getLastName() . "\n";
 foreach ($allGroups as $group) {
 	$t = $group->getGroupTypeRoleId();
 	$g = $group->getGroup();
 	echo $g->getTitle() . ' ';
-	echo '(ID: ' . $g->getDomainIdentifier() . " - " . ")\n";
+	echo '(ID: ' . $g->getDomainIdentifier() . ")\n";
 }
+echo "\n";
+
+$calApi = new CalendarApi($client, $config);
+echo "Alle sichtbaren Kalender: ";
+$cals = [];
+foreach ($calApi->getCalendars()->getData() as $cal) {
+	$cals[] = $cal->getName() . '(' . $cal->getId() . ')';
+}
+echo join(', ', $cals) . "\n\n";
+
+$campusApi = new CampusApi($client, $config);
+echo "Alle sichtbaren Campuses: ";
+$camps = [];
+foreach ($campusApi->getAllCampuses()->getData() as $c) {
+	$camps[] = $c->getName() . '(' . $c->getId() . ')';
+}
+echo join(', ', $camps) . "\n\n";
 
 
 $done = '';
